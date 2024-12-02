6f77697c5b66b6744c1be4250c67380e6be326a5
mikescriven <scrivenm01@gmail.com>
2024-12-02 06:57:29 +0000

    day 2

--- AutoDownloader/src/days/day_two.rs
+++ AutoDownloader/src/days/day_two.rs
@@ -6,33 +6,106 @@ pub struct DayTwo {
     day: i32
 }
 
-impl Problem<(Vec<i32>, Vec<i32>)> for DayTwo {
-    fn parse_input(&self, filename: &str) -> (Vec<i32>, Vec<i32>) {
+impl Problem<(Vec<Vec<i32>>)> for DayTwo {
+    fn parse_input(&self, filename: &str) -> (Vec<Vec<i32>>) {
         let file_path = format!("/Users/mikescriven/Code/AdventOfCode/scrivens_do_adventofcode/AutoDownloader/day{}/{}", self.day, filename);
 
         if !!!Path::new(&file_path).exists() {
             println!("Day 1 Part One, file doesn't exist: {}", file_path);
         }
 
-        let mut left_side = Vec::new();
-        let mut right_side = Vec::new();
-        //
-        // for line in read_to_string(file_path).unwrap().lines() {
-        //     let split_line : Vec<&str> = line.split("   ").collect();
-        //     left_side.push(split_line[0].parse().unwrap());
-        //     right_side.push(split_line[1].parse().unwrap());
-        // }
-        //
-        (left_side, right_side)
+        let mut reports = Vec::new();
+
+        for line in read_to_string(file_path).unwrap().lines() {
+            let mut report = Vec::new();
+            let splits = line.split(" ").collect::<Vec<&str>>();
+            println!("{:?}", splits);
+            let split_line: Vec<&str> = line.split(" ").collect();
+            println!("{:?}", split_line);
+            for item in split_line {
+                report.push(item.parse().unwrap());
+            }
+            reports.push(report);
+        }
+
+        (reports)
     }
 
-    fn part_one(&self, input: (Vec<i32>, Vec<i32>)) -> String {
-        String::from("empty question two")
+    fn part_one(&self, input: (Vec<Vec<i32>>)) -> String {
+        let mut sum = 0;
+        for report in input {
+            let mut index = 0;
+            while index < report.len() {
+                let mut copy = report.clone();
+                copy.remove(index);
+                let passed = check_report(copy);
+                if passed {
+                    sum += 1;
+                    break;
+                }
+                index += 1;
+            }
+        }
+
+        sum.to_string()
     }
 
-    fn part_two(&self, input: (Vec<i32>, Vec<i32>)) -> String {
+    fn part_two(&self, input: (Vec<Vec<i32>>)) -> String {
         String::from("empty question two")
     }
+}
+
+fn check_report(report: Vec<i32>) -> bool
+{
+    println!("START {:?}", report);
+    let mut copy = report.clone();
+    copy.sort();
+
+    let mut passed = 0;
+    if (report.eq(&copy)) {
+        passed += 1;
+    }
+
+    if ((passed == 0)) {
+        copy.reverse();
+        if (report.eq(&copy)) {
+            passed += 1;
+        }
+    }
+
+    if (passed == 0) {
+        println!("Failed order checks");
+        return false;
+    }
+
+    let mut index = 0;
+    let mut count = 0;
+    while index < (report.len() - 1) {
+        let num = report[index];
+        let next_num = report[index + 1];
+        let bigger_than_zero = (next_num - num).abs() > 0;
+        let smaller_than = (next_num - num).abs() < 4;
+
+        if (bigger_than_zero) {
+            if (smaller_than) {
+                count += 1;
+            } else {
+                println!("9");
+                return false;
+            }
+        } else {
+            println!("9");
+            return false;
+        }
+
+
+        index += 1;
+    }
+
+    if (count == report.len() - 1) {
+        return true;
+    }
+    return false;
 }
 
 #[cfg(test)]
@@ -41,11 +114,11 @@ mod tests {
 
     #[test]
     fn test_read_lines() {
-        let filename: &str = "demo1";
-        let day1 = DayTwo { day: 1 };
-        let output: (Vec<i32>, Vec<i32>) = day1.parse_input(filename);
-        assert_eq!(output.0, Vec::from([3, 4, 2, 1, 3, 3]));
-        assert_eq!(output.1, Vec::from([4, 3, 5, 3, 9, 3]));
+        let filename: &str = "demo";
+        let day1 = DayTwo { day: 2 };
+        let output: (Vec<Vec<i32>>) = day1.parse_input(filename);
+        assert_eq!(output[0], Vec::from([7, 6, 4, 2, 1]));
+        // assert_eq!(output.1, Vec::from([4, 3, 5, 3, 9, 3]));
     }
 
     #[test]
@@ -60,19 +133,30 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let filename: &str = "demo1";
-        let day1 = DayTwo { day: 1 };
+        let filename: &str = "demo";
+        let day1 = DayTwo { day: 2 };
 
         let input = day1.parse_input(filename);
         let result = day1.part_one(input);
 
-        assert_eq!(result, "11");
+        assert_eq!(result, "4");
     }
 
     #[test]
-    fn test_part_one_real() {
-        let filename: &str = "input1";
-        let day1 = DayTwo { day: 1 };
+    fn test_part_one_real_mike() {
+        let filename: &str = "input2";
+        let day1 = DayTwo { day: 2 };
+
+        let input = day1.parse_input(filename);
+        let result = day1.part_one(input);
+
+        assert_eq!(result, "blah");
+    }
+
+    #[test]
+    fn test_part_one_real_jo() {
+        let filename: &str = "input2jo";
+        let day1 = DayTwo { day: 2 };
 
         let input = day1.parse_input(filename);
         let result = day1.part_one(input);
@@ -82,7 +166,7 @@ mod tests {
 
     #[test]
     fn test_part_two_demo() {
-        let filename: &str = "demo1";
+        let filename: &str = "demo";
         let day1 = DayTwo { day: 1 };
 
         let input = day1.parse_input(filename);
